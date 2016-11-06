1002fd0a062020cdf5987abaedd7cd23f0f4042f
James Hurcomb <james@hurcomb.net>
2016-11-06 20:44:42 +0000

    Add call and response triggers to auto

--- plugins/auto.py
+++ plugins/auto.py
@@ -2,8 +2,14 @@ import json
 async def main(message, client, config, Discord):
 
     await addReaction(message, client, config, Discord) # Check for reactions
+    await automatedReply(message, client, config) # Check for automatedReplies
 
 async def addReaction(message, client, config, Discord):
     for react in config["reactions"]:
         if react["trigger"].lower() in message.content.lower():
             await client.add_reaction(message, react["react"])
+
+async def automatedReply(message, client, config):
+    for automatedReply in config["automatedReplies"]:
+        if automatedReply["trigger"].lower() in message.content.lower():
+            await client.send_message(message.channel, automatedReply["response"])
